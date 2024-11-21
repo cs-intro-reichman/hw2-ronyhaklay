b717ba2fe0abb285e0b575487167376d8799a3ae
רוני חקלאי <ronyhaklay@Air-sl-rwny.lan>
2024-11-22 00:58:31 +0200

    ex 3 cheers

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,24 @@
-// Prints a crowd cheering output.
+import javax.print.DocFlavor.STRING;
+
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String charsToCheers = (args[0]).toUpperCase();
+                int  count = Integer.parseInt(args[1]);
+                String needAnChars = "AEFHILMNORSX";
+
+                for (int i = 0 ; i < charsToCheers.length() ; i++) {
+                    char oneChar = charsToCheers.charAt(i);
+        
+                    if (needAnChars.indexOf(oneChar) >= 0) {
+                        System.out.println("Give me an " + oneChar + " : " + oneChar + "!");
+                    } else {
+                        System.out.println("Give me a " + oneChar + " : " + oneChar + "!");
+                    }
+                }
+
+                System.out.println("What does that spell?");
+
+                for (int j = 0 ; j < count ; j++)
+                System.out.println(charsToCheers + "!!!");
+            }
         }
-}
