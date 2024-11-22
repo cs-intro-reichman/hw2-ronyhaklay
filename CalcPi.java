9c6bf05473dfda645617eb3f894d3c120d212d70
רוני חקלאי <ronyhaklay@Air-sl-rwny.lan>
2024-11-22 23:53:11 +0200

    ex 4 calc pai!

--- CalcPi.java
+++ CalcPi.java
@@ -9,6 +9,6 @@ public class CalcPi {
 		}
 		pai *= 4;
 		System.out.println("pi according to Java: " + Math.PI);
-		System.out.println("pi, approximated: " + pai);
+		System.out.println("pi, approximated:     " + pai);
 	}
 }
