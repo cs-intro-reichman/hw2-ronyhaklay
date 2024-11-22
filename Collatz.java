71f22ca0f36b19401bf0e71cfbc3327e2347b624
רוני חקלאי <ronyhaklay@Air-sl-rwny.lan>
2024-11-23 01:39:23 +0200

    Ex 5 final

--- Collatz.java
+++ Collatz.java
@@ -4,21 +4,26 @@ public class Collatz {
         String mode = args[1];
 
         for (int i = 1; i <= seed; i++) {
-            int N = 1; 
-            int count = i; 
+            int N = i; 
+            int count = 0; 
             String step = "";
+
+     
             do {
                 step += N + " ";
                 N = (N % 2 == 0) ? N / 2 : N * 3 + 1;
                 count++;
-            } 
-            while (N != 1);
-            step += "1 (" + (count + 1) + ")";
+            } while (N != 1);
+
+            step += "1 (" + (count + 1) + ")"; 
+
+        
             if (mode.equals("v")) {
                 System.out.println(step);
+            }
         }
 
+       
         System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
     }
-}
 }
