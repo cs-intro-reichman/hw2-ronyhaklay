4c40496b7935f30731f32b483361211095978a26
רוני חקלאי <ronyhaklay@Air-sl-rwny.lan>
2024-11-23 01:01:54 +0200

    Ex 5 Collatz final!

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,6 @@
 public class Collatz {
     public static void main(String[] args) {
-		int N = Integer.parseInt(args[0]);
+        int N = Integer.parseInt(args[0]);
         String mode = args[1];
 
         if (!mode.equals("v") && !mode.equals("c")) {
@@ -9,17 +9,16 @@ public class Collatz {
         }
 
         for (int seed = 1; seed <= N; seed++) {
-            int countsteps = 0;
-            int current = seed;
-            String sequence = "";
-
+            int countSteps = 0; 
+            int current = seed; 
+            StringBuilder sequence = new StringBuilder(); 
             do {
-                if (sequence.isEmpty()) {
-                    sequence = Integer.toString(current);
+                if (sequence.length() == 0) {
+                    sequence.append(current);
                 } else {
-                    sequence += " " + current;
+                    sequence.append(" ").append(current);
                 }
-                countsteps++;
+                countSteps++;
                 if (current == 1) {
                     break;
                 } else if (current % 2 == 0) {
@@ -28,9 +27,8 @@ public class Collatz {
                     current = current * 3 + 1;
                 }
             } while (true);
-
             if (mode.equals("v")) {
-                System.out.println("     " + sequence + " (" + countsteps + ")");
+                System.out.println(sequence + " (" + countSteps + ")");
             }
         }
 
