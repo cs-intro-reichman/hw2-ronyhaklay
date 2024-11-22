68c82e13295266e3affb9a2507ac762765055314
רוני חקלאי <ronyhaklay@Air-sl-rwny.lan>
2024-11-23 00:19:07 +0200

    ex 5 collatz.java

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,27 @@
-// Demonstrates the Collatz conjecture.
+
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int N = Integer.parseInt(args[0]);
+		int seed;
+
+		for(seed = 1; seed <=N; seed++)
+		{
+			findSequence(seed);
+		}
+			System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+		
+	}
+	public static void findSequence(int s)
+	{
+		int current = s;
+		while (current != 1)
+		if(current % 2 ==0)
+		{
+		current /= 2; 
+		}
+		else
+		{
+			current = 3 * current + 1;
+		}
 	}
 }
