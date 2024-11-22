439a4e5a502b7cba73d6994a13a1256e88ae2b33
רוני חקלאי <ronyhaklay@Air-sl-rwny.lan>
2024-11-23 00:42:26 +0200

    ex 5 collatz java

--- Collatz.java
+++ Collatz.java
@@ -1,29 +1,26 @@
 public class Collatz {
     public static void main(String[] args) {
-        int N = Integer.parseInt(args[0]); 
+        int N = Integer.parseInt(args[0]);
 
         for (int seed = 1; seed <= N; seed++) {
-            findSequence(seed); 
-        }
-        System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
-    }
+            int current = seed;
+            int length = 1;
 
-    public static void findSequence(int s) {
-        int current = s;
-        int length = 0;
+            System.out.print(current);
 
-        System.out.print(current); 
-        length++;
-
-        while (current != 1) {
-            if (current % 2 == 0) {
-                current /= 2; 
-            } else {
-                current = 3 * current + 1; 
+            while (current != 1) {
+                if (current % 2 == 0) {
+                    current /= 2;
+                } else {
+                    current = 3 * current + 1;
+                }
+                System.out.print(" " + current);
+                length++;
             }
-            System.out.print(" " + current); 
-            length++;
+
+            System.out.println(" (" + length + ")");
         }
-        System.out.println(" (" + length + ")"); 
+
+        System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
     }
 }
